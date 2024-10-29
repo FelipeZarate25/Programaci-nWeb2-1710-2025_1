99b815b419ff9bd0c9816d04e3752d50497afe47
FelipeZarate25 <felipezarate11@aragon.unam.mx>
2024-10-28 18:29:05 -0600

    Tarea Rutas y paginas con node

---
+++ Tarea Rutas y paginas con node/rutas/app.js
@@ -0,0 +1,14 @@
+const express = require("express"); // Importa Express
+const path = require("path"); // Importa Path para manejar rutas de archivos
+const app = express(); // Crea una instancia de Express
+const port = 3010; // Define el puerto en el que se ejecutará el servidor
+const routes = require("./routes/server"); // Importa las rutas desde server.js
+
+// Configura Express para servir archivos estáticos desde la carpeta 'routes'
+app.use(express.static(path.join(__dirname, 'routes')));
+
+app.use("/", routes); // Usa las rutas definidas en server.js
+
+app.listen(port, () => {
+    console.log(`Servidor en escucha desde http://localhost:${port}`);
+});
